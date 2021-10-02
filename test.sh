5423b254e2936c5a690d7c732e54f63b45b5b2b5
Robert <geleg.rigzin@mail.ru>
2021-10-02 22:26:51 +0300

    robert_gaisin/problem_1

--- test.sh
+++ test.sh
@@ -2,15 +2,20 @@
 
 ./gen
 ./$1 a.bin b.bin out.bin 0
-echo "c"
+echo "cекунд"
 ./cntrl cntrl.bin out.bin
 ./$1 a.bin b.bin out.bin 1
+echo "cекунд"
 ./cntrl cntrl.bin out.bin
 ./$1 a.bin b.bin out.bin 2
+echo "cекунд"
 ./cntrl cntrl.bin out.bin
 ./$1 a.bin b.bin out.bin 3
+echo "cекунд"
 ./cntrl cntrl.bin out.bin
 ./$1 a.bin b.bin out.bin 4
+echo "cекунд"
 ./cntrl cntrl.bin out.bin
 ./$1 a.bin b.bin out.bin 5
+echo "cекунд"
 ./cntrl cntrl.bin out.bin
